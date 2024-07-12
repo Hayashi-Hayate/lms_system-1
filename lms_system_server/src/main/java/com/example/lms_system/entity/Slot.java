81fdc4ef4b2e151b6bbac72a40b406bff7456a17
lampthe162003 <lampthe162003@fpt.edu.vn>
2024-07-12 22:13:43 +0700

    add @data

--- lms_system_server/src/main/java/com/example/lms_system/entity/Slot.java
+++ lms_system_server/src/main/java/com/example/lms_system/entity/Slot.java
@@ -9,8 +9,10 @@ import jakarta.persistence.Id;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Data
 @Entity
 @Builder
 @AllArgsConstructor
